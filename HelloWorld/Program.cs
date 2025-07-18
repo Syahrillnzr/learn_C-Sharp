9890e8c0b6d5e4371fa59be05e363f6a0dedf196
matRill <syahrill.dev@gmail.com>
2025-07-18 15:44:50 +0800

    learn switch statement

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -317,6 +317,50 @@ namespace HelloWorld
             int timee = 20;
             string result = (timee < 18) ? "Good day." : "Good evening.";
             Console.WriteLine(result);
+
+            Console.WriteLine("========================");
+            Console.WriteLine(" ");
+
+
+            // C# Switch
+            Console.WriteLine(" This is C# Switch");
+            Console.WriteLine(" ");
+
+            int day = 8;
+            switch (day)
+            {
+                case 1:
+                Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thurday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Looking forward to the Weekend.");
+                    break;
+
+            }
+
+            Console.WriteLine("========================");
+            Console.WriteLine(" ");
+
+
+
         }
     }
 
